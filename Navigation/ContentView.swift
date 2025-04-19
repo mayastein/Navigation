1570e3988ca8bcd62089a2e612886a6bd9d6b269
Maya Stein <mayarosestein@gmail.com>
2025-04-19 17:06:52 -0400

    Added a navigation stack with 1 more view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Root View :)")
+                NavigationLink(destination: Text("Welcome to view 2")) {
+                    Text("Click Me")
+                }
+            }
         }
-        .padding()
     }
 }
 
